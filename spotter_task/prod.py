c3b597cd939f270f08187d610ff69894f777ac13
karoatani <karoatani@gmail.com>
2025-07-31 07:47:41 +0100

    commit

--- spotter_task/prod.py
+++ spotter_task/prod.py
@@ -1,6 +1,6 @@
 from settings import *
 from decouple import config
-
+import dj_database_url
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 DEBUG = False
@@ -9,3 +9,7 @@ DEBUG = False
 
 CORS_ALLOW_ALL_ORIGINS = config("CORS_ALLOW_ALL_ORIGINS", default=False, cast=bool)
 
+
+DATABASES = {
+    'default': dj_database_url.config(),
+}
